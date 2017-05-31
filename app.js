a5ac484b298962cdef593dd61d62a90d0e841313
rednerank <narender.kumar@talentica.com>
2017-05-31 16:55:33 +0530

    added pages

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
 
 //routes
-app.get('/',function(req, res){
+/*app.get('/',function(req, res){
 	res.send('Hello World!');
+});*/
+
+router.use(function (req, res, next) {
+	console.log("/"+req.method);
+	next();
+});
+
+router.get("/", function(req, res){
+	res.sendFile(path + "index.html")
+});
+
+router.get("/about", function(req, res){
+    res.sendFile(path + "about.html")
+});
+
+router.get("/contact", function(req, res){
+    res.sendFile(path + "contact.html")
+});
+
+router.get("/timetable", function(req, res){
+    res.sendFile(path + "timetable.html")
+});
+
+app.use("/",router);
+
+app.use("*", function (req, res) {
+	res.sendFile(path + "404.html")
 });
 
 // Listen
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log('listening on localhost:'+ port);
+app.listen(3000,function(){
+    console.log("Live at Port 3000");
+});
